cd715aac033f60dc4e0d5c0869925d47a5b8c9fb
anderson949 <138420698+anderson949@users.noreply.github.com>
2024-08-07 12:36:33 -0300

    Update stable_api.py

--- iqoptionapi/stable_api.py
+++ iqoptionapi/stable_api.py
@@ -398,18 +398,18 @@ class IQ_Option:
         for actives in init_info["result"]["turbo"]["actives"]:
             name = init_info["result"]["turbo"]["actives"][actives]["name"]
             name = name[name.index(".") + 1:len(name)]
-            all_profit[name]["turbo"] = ((
+            all_profit[name]["turbo"] = (
                 100.0 -
                 init_info["result"]["turbo"]["actives"][actives]["option"]["profit"][
-                    "commission"])-1) / 100.0
+                    "commission"]) / 100.0
 
         for actives in init_info["result"]["binary"]["actives"]:
             name = init_info["result"]["binary"]["actives"][actives]["name"]
             name = name[name.index(".") + 1:len(name)]
-            all_profit[name]["binary"] = (
+            all_profit[name]["binary"] = ((
                 100.0 -
                 init_info["result"]["binary"]["actives"][actives]["option"]["profit"][
-                    "commission"]) / 100.0
+                    "commission"]) / 100.0)-1
         return all_profit
 
     # ----------------------------------------
