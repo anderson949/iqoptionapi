247c30380a6c6b91ddc8d2e1d8e294449262889b
anderson949 <138420698+anderson949@users.noreply.github.com>
2024-11-30 18:31:07 -0300

    Update billing.py

--- iqoptionapi/http/billing.py
+++ iqoptionapi/http/billing.py
@@ -1,10 +1,7 @@
-"""Module for IQ option billing resource."""
 
 from iqoptionapi.http.resource import Resource
 
 
 class Billing(Resource):
-    """Class for IQ option billing resource."""
-    # pylint: disable=too-few-public-methods
 
     url = "billing"
