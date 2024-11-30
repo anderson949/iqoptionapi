1aef904f50497e1f72d1150bcf2a144ad227fed3
anderson949 <138420698+anderson949@users.noreply.github.com>
2024-11-30 18:24:48 -0300

    Update loginv2.py

--- iqoptionapi/http/loginv2.py
+++ iqoptionapi/http/loginv2.py
@@ -1,11 +1,8 @@
-"""Module for IQ Option http loginv2 resource."""
 
 from iqoptionapi.http.login import Login
 
 
 class Loginv2(Login):
-    """Class for IQ option loginv2 resource."""
-    # pylint: disable=too-few-public-methods
 
     url = "/".join((Login.url, "v2"))
 
