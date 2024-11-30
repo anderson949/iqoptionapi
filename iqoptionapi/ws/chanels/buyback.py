8078e4406fb6cabbee58b1a44713a2c970fa160e
anderson949 <138420698+anderson949@users.noreply.github.com>
2024-11-30 18:50:45 -0300

    Update buyback.py

--- iqoptionapi/ws/chanels/buyback.py
+++ iqoptionapi/ws/chanels/buyback.py
@@ -1,11 +1,7 @@
-"""Module for IQ Option buyback websocket chanel."""
-
 from iqoptionapi.ws.chanels.base import Base
 
 
 class Buyback(Base):
-    """Class for IQ option subscribe to buyback websocket chanel."""
-    # pylint: disable=too-few-public-methods
 
     name = "buyback"
 
