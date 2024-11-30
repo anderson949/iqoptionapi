503e15fb38b1ce655404583b84d8933512bd41c0
anderson949 <138420698+anderson949@users.noreply.github.com>
2024-11-30 18:24:00 -0300

    Update logout.py

--- iqoptionapi/http/logout.py
+++ iqoptionapi/http/logout.py
@@ -1,20 +1,13 @@
-"""Module for IQ Option http login resource."""
-
 from iqoptionapi.http.resource import Resource
 
 
 class Logout(Resource):
-    """Class for IQ option login resource."""
-    # pylint: disable=too-few-public-methods
 
     url = ""
 
     def _post(self, data=None, headers=None):
-        """Send get request for IQ Option API login http resource.
 
-        :returns: The instance of :class:`requests.Response`.
-        """
-        return self.api.send_http_request_v2(method="POST", url="https://auth.iqoption.com/api/v1.0/logout",data=data, headers=headers)
+        return self.api.send_http_request_v2(method="POST", url=f"https://auth.{self.api.host}/api/v1.0/logout",data=data, headers=headers)
 
     def __call__(self):
        
