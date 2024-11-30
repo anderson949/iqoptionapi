2f062edc6b048e778a4ce5744806cd53ab35bdb0
anderson949 <138420698+anderson949@users.noreply.github.com>
2024-11-30 18:31:47 -0300

    Update auth.py

--- iqoptionapi/http/auth.py
+++ iqoptionapi/http/auth.py
@@ -1,10 +1,7 @@
-"""Module for IQ Option http auth resource."""
 
 from iqoptionapi.http.resource import Resource
 
 
 class Auth(Resource):
-    """Class for IQ Option http auth resource."""
-    # pylint: disable=too-few-public-methods
 
     url = "auth"
