825d084d138e7097960a144e67e19d0ba041ff99
anderson949 <138420698+anderson949@users.noreply.github.com>
2024-12-26 11:50:38 -0300

    Update stable_api.py

--- iqoptionapi/stable_api.py
+++ iqoptionapi/stable_api.py
@@ -66,44 +66,44 @@ class IQ_Option:
             # Inicializa a API com as credenciais do usuário
             self.api = IQOptionAPI("iqoption.com", self.email, self.password)
     
-        # Verifica se a autenticação de dois fatores (2FA) é necessária
-        if sms_code is not None:
-            self.api.setTokenSMS(self.resp_sms)
-            status, reason = self.api.connect2fa(sms_code)
-            if not status:
-                return status, reason
-    
-        # Configura os cabeçalhos e cookies da sessão
-        self.api.set_session(headers=self.SESSION_HEADER, cookies=self.SESSION_COOKIE)
-    
-        # Tenta estabelecer a conexão
-        check, reason = self.api.connect()
-    
-        if check:
-            # Reconecta aos streams de dados previamente inscritos
-            self.re_subscribe_stream()
-    
-            # Aguarda até que o balance_id esteja disponível
-            while global_value.balance_id is None:
-                time.sleep(0.1)  # Evita o uso excessivo da CPU
-    
-            # Inscreve-se para receber atualizações de posições e ordens
-            self.position_change_all("subscribeMessage", global_value.balance_id)
-            self.order_changed_all("subscribeMessage")
-            self.api.setOptions(1, True)
-    
-            return True, None
-        else:
-            # Verifica se a falha foi devido à necessidade de autenticação de dois fatores (2FA)
-            if json.loads(reason)['code'] == 'verify':
-                response = self.api.send_sms_code(json.loads(reason)['token'])
-                if response.json()['code'] != 'success':
-                    return False, response.json()['message']
-    
-                # Armazena a resposta do SMS para uso futuro
-                self.resp_sms = response
-                return False, "2FA"
-            return False, reason
+            # Verifica se a autenticação de dois fatores (2FA) é necessária
+            if sms_code is not None:
+                self.api.setTokenSMS(self.resp_sms)
+                status, reason = self.api.connect2fa(sms_code)
+                if not status:
+                    return status, reason
+        
+            # Configura os cabeçalhos e cookies da sessão
+            self.api.set_session(headers=self.SESSION_HEADER, cookies=self.SESSION_COOKIE)
+        
+            # Tenta estabelecer a conexão
+            check, reason = self.api.connect()
+        
+            if check:
+                # Reconecta aos streams de dados previamente inscritos
+                self.re_subscribe_stream()
+        
+                # Aguarda até que o balance_id esteja disponível
+                while global_value.balance_id is None:
+                    time.sleep(0.1)  # Evita o uso excessivo da CPU
+        
+                # Inscreve-se para receber atualizações de posições e ordens
+                self.position_change_all("subscribeMessage", global_value.balance_id)
+                self.order_changed_all("subscribeMessage")
+                self.api.setOptions(1, True)
+        
+                return True, None
+            else:
+                # Verifica se a falha foi devido à necessidade de autenticação de dois fatores (2FA)
+                if json.loads(reason)['code'] == 'verify':
+                    response = self.api.send_sms_code(json.loads(reason)['token'])
+                    if response.json()['code'] != 'success':
+                        return False, response.json()['message']
+        
+                    # Armazena a resposta do SMS para uso futuro
+                    self.resp_sms = response
+                    return False, "2FA"
+                return False, reason
     
     def get_server_timestamp(self):
         """
